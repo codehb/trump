958154ac0286fb4e3cca63a7a393d3d69385c19c
codehb <dave@dave.com>
2019-03-28 16:33:42 +0000

    Trump Files

---
+++ index.js
@@ -0,0 +1,25 @@
+var can = document.getElementById("canvas"),
+    ctx = can.getContext('2d'),
+    img = new Image();
+
+img.src = 'trump.png';
+can.onclick= function(evt) {
+    var x = evt.offsetX - img.width/2,
+        y = evt.offsetY - img.height/2;
+
+    ctx.drawImage(img, x, y);
+};
+can.onmousemove= function(evt) {
+    var x = evt.offsetX - img.width/2,
+        y = evt.offsetY - img.height/2;
+
+    ctx.drawImage(img, x, y);
+};
+function resize() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	render();
+}
+window.addEventListener('resize', resize, false); resize();
+function render() { // draw to screen here
+}
